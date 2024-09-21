be57f30e8e4a3cdeae0c1724485bae301b78ee88
rahul <nizarerahul7@gmail.com>
2024-09-21 22:21:26 +0530

    condtional statements

---
+++ Javascript/WD - JavaScript Essentials & Advanced/05_conditionalStatment.js
@@ -0,0 +1,10 @@
+// What is Condition Statement?
+
+/* conditional statments in javasript allow us to control the code base on condition wheater its true or false.valueOf */
+
+if(2>1){
+    console.log(true);
+}
+else{
+    console.log(false)
+}
