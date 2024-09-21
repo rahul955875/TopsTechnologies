54bed2e023284a7154b7731d912feebce291a767
rahul <nizarerahul7@gmail.com>
2024-09-21 19:46:22 +0530

    date

---
+++ Javascript/WD - JavaScript Essentials & Advanced/03_areaOftriangle.js
@@ -0,0 +1,6 @@
+// Q.3 Write a JavaScript Program to find the area of a triangle?
+
+function areaTri(base,height){
+    console.log((base*height)/2);
+}
+areaTri(10,20);
---
+++ Javascript/WD - JavaScript Essentials & Advanced/04_calculateDays.js
@@ -0,0 +1,9 @@
+/* Q.4 Write a JavaScript program to calculate days left until next Christmas? */
+
+let d = new Date();
+let date = d.getDate();
+let month = d.getMonth();
+let year = d.getFullYear();
+
+console.log(d)
+console.log(typeof year);
