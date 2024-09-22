bfd10e1deebbb7bbbcc347c01ea2b72b9155758d
rahul <nizarerahul7@gmail.com>
2024-09-22 22:23:02 +0530

    check positive or negative

---
+++ Javascript/WD - JavaScript Essentials & Advanced/13_checkNumberPositiveorNeg.js
@@ -0,0 +1,11 @@
+// Q.13 Check Number Is Positive or Negative in JavaScript?
+let num = 0;
+if(num > 0){
+    console.log("numbver is positive");
+}
+else if(num===0){
+    console.log("number is zero")
+}
+else{
+    console.log("number is negative");
+}
