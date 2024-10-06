1859faf2f6394e7fabe8c3b0ae87ad2a9f64bc46
rahul <nizarerahul7@gmail.com>
2024-10-06 19:08:11 +0530

    meeting section

--- Designs-(Class)/Assessment - Website Designing/app.js
+++ Designs-(Class)/Assessment - Website Designing/app.js
@@ -4,11 +4,5 @@ const btn2 = document.querySelector(".btn2");
 const box  = document.querySelectorAll(".box");
 
 btn1.addEventListener("click",()=>{
- box.forEach(b =>{
-    //  b.style.animationPlayState = "paused";
-    //  b.style.animationFillMode = "forwards";
-    b.style.animation = "none"
-    b.offsetHeight;
-     b.style.transform = "translateX(-100px)";
- })
+ 
 })
