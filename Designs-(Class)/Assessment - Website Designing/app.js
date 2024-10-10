93a19d03a9b2f8e41573cec2fc29cc2f9036d8f1
rahul <nizarerahul7@gmail.com>
2024-10-10 19:01:40 +0530

    new changes

--- Designs-(Class)/Assessment - Website Designing/app.js
+++ Designs-(Class)/Assessment - Website Designing/app.js
@@ -1,11 +1,16 @@
 // console.log("running")
-// const btn1 = document.querySelector(".btn1");
-// const btn2 = document.querySelector(".btn2");
-// const box  = document.querySelectorAll(".box");
-
-// btn1.addEventListener("click",()=>{
-
-// })
+const btn1 = document.querySelector(".btn1");
+const btn2 = document.querySelector(".bnt2");
+const boxes = document.querySelectorAll(".service-box");
+// let itemSet = boxes.length;
+btn1.addEventListener("click", () => {
+   boxes.forEach(e =>{ e.style.animation = "none"})
+})
+btn2.addEventListener("click", ()=>{
+  boxes.forEach(e =>{
+    e.style.animation = "slider 20s cubic-bezier(0.165, 0.84, 0.44, 1) infinite"
+  })
+})
 
 const icon1 = document.querySelector("#icon1");
 const icon2 = document.querySelector("#icon2");
@@ -34,30 +39,42 @@ function dropDown(icon, p) {
   flag = !flag;
 }
 
+const wrapper = document.querySelector(".course-box-wrapper");
+const items = document.querySelectorAll(".owl-item");
+const prevBtn = document.querySelector(".c-btn:first-child");
+const nextBtn = document.querySelector("#next-btn");
 
-const wrapper = document.querySelector('.course-box-wrapper');
-const items = document.querySelectorAll('.owl-item');
-const prevBtn = document.querySelector('.c-btn:first-child');
-const nextBtn = document.querySelector('#next-btn');
-
-let currentIndex = 0; 
-const totalItems = items.length -3; 
-
+let currentIndex = 0;
+const totalItems = items.length - 3;
 
 function updateSlider() {
   const width = items[0].clientWidth + 30;
-  wrapper.style.transform = `translateX(-${(currentIndex * width)}px)`;
+  wrapper.style.transform = `translateX(-${currentIndex * width}px)`;
 }
 
-nextBtn.addEventListener('click', () => {
-
+nextBtn.addEventListener("click", () => {
   currentIndex = (currentIndex + 1) % totalItems;
   updateSlider();
 });
 
-prevBtn.addEventListener('click', () => {
+prevBtn.addEventListener("click", () => {
   currentIndex = (currentIndex - 1 + totalItems) % totalItems;
   updateSlider();
 });
 
 // updateSlider();
+
+// ======menu-icon===============
+const menuIcon = document.querySelector(".menu-icon");
+const navSm = document.querySelector("#nav-sm");
+let flagSm = true;
+menuIcon.addEventListener("click", () => {
+  if (flagSm) {
+    navSm.style.transform = "translateX(0)";
+    menuIcon.innerHTML = "close";
+  } else {
+    navSm.style.transform = "translateX(100%)";
+    menuIcon.innerHTML = "menu";
+  }
+  flagSm = !flagSm;
+});
