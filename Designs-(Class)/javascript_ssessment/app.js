912f03dc82f8deb85c8213ea78eb32ea87263f59
rahul <nizarerahul7@gmail.com>
2024-10-11 14:58:45 +0530

    new changes

--- Designs-(Class)/javascript_ssessment/app.js
+++ Designs-(Class)/javascript_ssessment/app.js
@@ -2,19 +2,32 @@ const input = document.querySelector("#input");
 const add = document.querySelector("#Add");
 const container = document.querySelector(".container-body");
 
-add.addEventListener("click", ()=>{
-    let todo = input.value.trim();
-    if(todo){
-        let newNode = document.createElement("li")
-        
-        container.appendChild(newNode)
-        let delBtn = document.createElement("button")
-        delBtn.innerHTML = "&times;"
-        delBtn.className = "deleBtn"
-        newNode.textContent = todo;
-        container.appendChild(delBtn) 
-    }
-    else{
-        alert("enter your todays task")
-    }
-})
+add.addEventListener("click", () => {
+  let todo = input.value.trim();
+  if (todo) {
+    let newNode = document.createElement("li");
+
+    container.appendChild(newNode); 
+
+    let delBtn = document.createElement("button");
+    delBtn.innerHTML = "&times;";
+    delBtn.className = "deleBtn";
+    newNode.className = "node";
+    newNode.textContent = todo;
+    container.appendChild(delBtn);
+
+    input.value = "";
+
+    let cancel = document.querySelectorAll(".deleBtn");
+    let nodes  = document.querySelectorAll(".node");
+
+    cancel.forEach((e , i) => {
+        e.addEventListener("click" , ()=>{  
+            container.removeChild(cancel[i])
+        container.removeChild(nodes[i])
+        })
+    });
+  } else {
+    alert("enter your todays task");
+  }
+});
