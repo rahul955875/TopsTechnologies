88e40cfd1743990c385aa03163e5af61e465bd81
rahul <nizarerahul7@gmail.com>
2024-10-11 22:25:37 +0530

    looping structure

---
+++ Javascript/WD - JavaScript Essentials & Advanced/21_loopingStructure.js
@@ -0,0 +1,13 @@
+// Q.21 What are the looping structures in JavaScript? Any one Example?
+
+/* Looping Structures in JavaScript:
+for loop: Repeats a block of code a specific number of times.
+while loop: Executes a block of code as long as a specified condition is true.
+do...while loop: Executes a block of code once, and then repeats the loop as long as a specified condition is true.
+for...in loop: Iterates over the properties of an object.
+for...of loop: Iterates over iterable objects like arrays, strings, maps, etc. */
+
+// example =>
+    for(let i=1; i<=10; i++){
+        console.log(i)
+    }
