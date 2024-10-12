2fef6b03931a7c238ca739cd69bf3aa8b2fa5251
rahul <nizarerahul7@gmail.com>
2024-10-12 15:07:42 +0530

    reverse any number

--- Javascript/WD - JavaScript Essentials & Advanced/25_reverseNum.js
+++ Javascript/WD - JavaScript Essentials & Advanced/25_reverseNum.js
@@ -1,4 +1,7 @@
 // Q.25 Write to print number in reverse order e.g.: number = 64728 ---> reverse =82746 in JS?
-let num = "64728";
-let numarray = num.split("");
-console.log(numarray.reverse().join())
+let num = 64728;
+let numarray = num.toString().split("");
+let con = (numarray.reverse().join(""))
+let rev = Number(con)
+// console.log(typeof con)
+console.log(rev)
