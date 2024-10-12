a75a1c5b8ea2e6292e0837990f8054c261af4432
rahul <nizarerahul7@gmail.com>
2024-10-12 15:16:57 +0530

    summation

--- Javascript/WD - JavaScript Essentials & Advanced/26_summation.js
+++ Javascript/WD - JavaScript Essentials & Advanced/26_summation.js
@@ -1,4 +1,4 @@
 // Q.26 Write a program make a summation of given number (E.g., 1523 Ans: - 11) in JS?
 let num = 1523;
-let numarray = num.toString().split("").map(Number).reduce((acc,c)=>acc=acc+c);
+let numarray = num.toString().split("").reduce((acc,c)=>acc=acc+ Number(c),0);
 console.log(numarray)
