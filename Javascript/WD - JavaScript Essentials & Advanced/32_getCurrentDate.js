8a37320bc4d803b71f7594af59534ff8d1d7a645
rahul <nizarerahul7@gmail.com>
2024-10-13 21:40:05 +0530

    get time

--- Javascript/WD - JavaScript Essentials & Advanced/32_getCurrentDate.js
+++ Javascript/WD - JavaScript Essentials & Advanced/32_getCurrentDate.js
@@ -1,3 +1,8 @@
 // Q.32 Write a JavaScript program to get the current date?
 let currentDate = new Date();
+console.log(currentDate);
 console.log(currentDate.toLocaleDateString())
+console.log(currentDate.toDateString())
+console.log(currentDate.toLocaleString())
+// console.log(currentDate.toISOString())
+console.log(currentDate.toLocaleTimeString())
