7ea556fa4b1e0e122070dcf182dd01dde9780c09
rahul <nizarerahul7@gmail.com>
2024-10-17 21:24:01 +0530

    isArray

---
+++ Javascript/WD - JavaScript Essentials & Advanced/45_isArray.js
@@ -0,0 +1,8 @@
+let arr1 = ["rahul","nizare"];
+let num  = 1;
+
+console.log(Array.isArray(arr1));
+console.log(Array.isArray(num));
+
+console.log(arr1 instanceof Array)
+console.log(num instanceof Array)
