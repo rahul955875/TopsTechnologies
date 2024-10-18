f56ec26c399c4f6151c6f07eb2afb0f37af46580
rahul <nizarerahul7@gmail.com>
2024-10-18 19:27:40 +0530

    includes()

---
+++ Javascript/WD - JavaScript Essentials & Advanced/63_includes.js
@@ -0,0 +1,4 @@
+// Q.63 Check if the string contains a word Script using includes() method?
+const str = "javaScript is awesome";
+console.log(str.includes("Script"))
+console.log(str.includes("script"))
