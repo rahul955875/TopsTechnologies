69802331e9a0461759d716d545e2694b638d331d
rahul <nizarerahul7@gmail.com>
2024-10-18 13:30:30 +0530

    lastIndexof

---
+++ Javascript/WD - JavaScript Essentials & Advanced/53_lastindexOf.js
@@ -0,0 +1,4 @@
+// Q,53 Use lastIndexOf to determine the position of the last occurrence of a in 30 Days Of JavaScript?
+let str = "30 days of javaSript";
+let position = str.lastIndexOf('a');
+console.log(position)
