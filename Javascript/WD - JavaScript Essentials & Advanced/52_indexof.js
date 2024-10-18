6d12bb577f8c541866e4e0419a22d06c178ceb3d
rahul <nizarerahul7@gmail.com>
2024-10-18 13:28:06 +0530

    indexOf

---
+++ Javascript/WD - JavaScript Essentials & Advanced/52_indexof.js
@@ -0,0 +1,4 @@
+// Q.52 Use indexOf to determine the position of the first occurrence of a in 30 Days Of JavaScript?
+let str = "30 days of javasript";
+let position = str.indexOf('a');
+console.log(position)
