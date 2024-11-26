24296ad71803d8049d8f714ff44d7ff6354037bb
rahul <nizarerahul7@gmail.com>
2024-11-26 21:35:38 +0530

    create array using .from method

---
+++ Javascript/Advance js/02_arrayMethod.js
@@ -0,0 +1,7 @@
+let arr = 'js is programming language'
+let newarr = Array.from(arr) //create a new array 
+console.log(newarr)
+console.log(typeof newarr)
+
+var str = Array.from(["html", "css", "js"])
+console.log(str);
