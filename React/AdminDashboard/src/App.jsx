5ee334b31c71e5cdc8be6cf7dad0a76e41c2d961
rahul <nizarerahul7@gmail.com>
2025-02-19 23:13:47 +0530

    implement edit feature in product table - admin can update product details why model

--- React/AdminDashboard/src/App.jsx
+++ React/AdminDashboard/src/App.jsx
@@ -1,5 +1,5 @@
 import "bootstrap/dist/css/bootstrap.min.css";
-// import 'bootstrap/dist/js/bootstrap.bundle.js'
+import 'bootstrap/dist/js/bootstrap.bundle.js'
 
 import "./App.css";
 import AdminLogin from "./components/AdminLogin";
--- React/AdminDashboard/src/components/ShowProducts.jsx
+++ React/AdminDashboard/src/components/ShowProducts.jsx
@@ -1,13 +1,41 @@
 import React, { useState } from "react";
 
+
 const ShowProducts = () => {
+  const categoryList = ["Cameras", "Mobiles", "Laptops", "Accesories"];
   const getData = JSON.parse(localStorage.getItem("ProductData")) || [];
- const [productData , setProductData] = useState(getData)
+  const [productData, setProductData] = useState(getData);
   console.log("rendering");
-  
-  const handleDeleteProduct = (id) =>{
-    const filterData = productData.filter(product => product.id !== id)
-    setProductData(filterData)
+  localStorage.setItem('ProductData',JSON.stringify(productData))
+  const handleDeleteProduct = (id) => {
+    const filterData = productData.filter((product) => product.id !== id);
+    setProductData(filterData);
+    localStorage.setItem("ProductData", JSON.stringify(filterData));
+  };
+  const [proUpdatedData, setProUpdatedData] = useState({
+    id: "",
+      title: "",
+      category: "",
+      description: "",
+      oldPrice: "",
+      rate: "",
+      newPrice: "",
+      image: "",
+  })
+   const [fetchRealProData, setFetchRealProData] = useState({}) ;
+  const handleEdit = (fetchProductData) =>{
+    setProUpdatedData(fetchProductData)
+    setFetchRealProData(fetchProductData)
+    console.log(fetchRealProData)
+  }
+  const handleChange = (e)=>{
+  const existingProData = {...proUpdatedData, [e.target.name] : e.target.value}  
+  setProUpdatedData(existingProData)
+  }
+  const handleSubmit = (e)=>{
+    e.preventDefault()
+    setProductData(prevData => prevData.map(product => product.id === fetchRealProData.id ? proUpdatedData : product))
+    console.log(productData)
   }
   return (
     <>
@@ -21,6 +49,7 @@ const ShowProducts = () => {
               <th>Description</th>
               <th>Price</th>
               <th>Image</th>
+              <th>Action</th>
             </tr>
           </thead>
           <tbody>
@@ -42,12 +71,164 @@ const ShowProducts = () => {
                   />
                 </td>
                 <td>
-                  <button className="btn btn-danger" onClick={()=> handleDeleteProduct(product.id)}>Delete</button>
+                  <button
+                    className="btn btn-primary"
+                    data-bs-toggle="modal"
+                    data-bs-target="#updateProduct"
+                    onClick={()=>handleEdit(product)}
+                  >
+                    Edit
+                  </button>
+                </td>
+                <td>
+                  <button
+                    className="btn btn-danger"
+                    onClick={() => handleDeleteProduct(product.id)}
+                  >
+                    Delete
+                  </button>
                 </td>
               </tr>
             ))}
           </tbody>
         </table>
+      </div>
+      {/* model */}
+
+      <div
+        className="modal fade"
+        id="updateProduct"
+        tabIndex="-1"
+        aria-labelledby="updateProductLabel"
+        aria-hidden="true"
+      >
+        <div className="modal-dialog">
+          <div className="modal-content">
+            <div className="modal-header">
+              <h1 className="modal-title fs-5" id="updateProductLabel">
+                Update Product Details
+              </h1>
+              <button
+                type="button"
+                className="btn-close"
+                data-bs-dismiss="modal"
+                aria-label="Close"
+              ></button>
+            </div>
+            <div className="modal-body">
+              <form className="pt-3" onSubmit={handleSubmit}>
+                <div className="row g-3">
+                  <div className="col-4">
+                    <input
+                      type="text"
+                      placeholder="Enter Product Id"
+                      className="form-control"
+                      name="id"
+                      value={proUpdatedData.id}
+                      onChange={handleChange}
+                    />
+                    {/* <p className="text-danger">{error.id}</p> */}
+                  </div>
+                  <div className="col-4">
+                    <select
+                      name="category"
+                      value={proUpdatedData.category}
+                      onChange={handleChange}
+                      className="form-select"
+                    >
+                      <option value="" hidden>
+                        --select Category
+                      </option>
+                      {categoryList.map((option) => (
+                        <option key={option} value={option}>
+                          {option}
+                        </option>
+                      ))}
+                    </select>
+                    {/* <p className="text-danger">{error.category}</p> */}
+                  </div>
+                  <div className="col-4">
+                    <input
+                      type="text"
+                      placeholder="Title"
+                      className="form-control"
+                      name="title"
+                      value={proUpdatedData.title}
+                      onChange={handleChange}
+                    />
+                    {/* <p className="text-danger">{error.title}</p> */}
+                  </div>
+                  <div className="col-12">
+                    <textarea
+                      placeholder="Description"
+                      className="form-control"
+                      name="description"
+                      value={proUpdatedData.description}
+                      onChange={handleChange}
+                    ></textarea>
+                    {/* <p className="text-danger">{error.description}</p> */}
+                  </div>
+                  <div className="col-4">
+                    <input
+                      type="number"
+                      placeholder="Old Price"
+                      className="form-control"
+                      name="oldPrice"
+                      value={proUpdatedData.oldPrice}
+                      onChange={handleChange}
+                    />
+                    {/* <p className="text-danger">{error.oldPrice}</p> */}
+                  </div>
+                  <div className="col-4">
+                    <input
+                      type="number"
+                      placeholder="Discount (%)"
+                      className="form-control"
+                      name="rate"
+                      value={proUpdatedData.rate}
+                      onChange={handleChange}
+                    />
+                    {/* <p className="text-danger">{error.rate}</p> */}
+                  </div>
+                  <div className="col-12">
+                    <input
+                      type="text"
+                      placeholder="New Price"
+                      className="form-control"
+                      name="newPrice"
+                      value={proUpdatedData.newPrice}
+                      readOnly
+                    />
+                  </div>
+                  <div className="col-12">
+                    <input
+                      type="text"
+                      placeholder="Image URL"
+                      className="form-control"
+                      name="image"
+                      value={proUpdatedData.image}
+                      onChange={handleChange}
+                    />
+                    {/* <p>{error.image}</p> */}
+                  </div>
+                  
+                </div>
+            <div className="modal-footer">
+            <button
+                type="button"
+                className="btn btn-secondary"
+                data-bs-dismiss="modal"
+              >
+                Close
+              </button>
+              <button type="submit" className="btn btn-primary">
+                      Update
+                    </button>
+            </div>
+              </form>
+            </div>
+          </div>
+        </div>
       </div>
     </>
   );
