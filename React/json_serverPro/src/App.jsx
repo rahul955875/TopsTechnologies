ce6f98c2b0f2569bc1fbe592a33489c3194acdf2
rahul <nizarerahul7@gmail.com>
2025-02-25 14:39:01 +0530

    implement sort using emp id

--- React/json_serverPro/src/App.jsx
+++ React/json_serverPro/src/App.jsx
@@ -8,24 +8,26 @@ import AddDataToApi from "./components/AddDataToApi";
 
 function App() {
   const [data, setData] = useState([]);
-  const [isEditing, setIsEditing] = useState('')
+  const [isEditing, setIsEditing] = useState("");
   const fetchData = () => {
     fetch("http://localhost:3000/employee")
       .then((res) => res.json())
-      .then((data) => setData(data));
+      .then((data) => {
+        setData(data.sort((a,b)=>a.id - b.id));
+
+      });
   };
   useEffect(() => {
     fetchData();
   }, []);
 
-  const deleteData = (id)=>{
-    if(confirm("You want to delete this record ?")){
-      fetch(`http://localhost:3000/employee/${id}`,{
-        method: 'DELETE'
-      })
-      .then(res => fetchData())
+  const deleteData = (id) => {
+    if (confirm("You want to delete this record ?")) {
+      fetch(`http://localhost:3000/employee/${id}`, {
+        method: "DELETE",
+      }).then((res) => fetchData());
     }
-  }
+  };
   const [input, setInput] = useState({
     id: "",
     ename: "",
@@ -35,13 +37,17 @@ function App() {
     profile: "",
   });
 
-  const editData = (emp) =>{
-    setIsEditing(emp.id)
-    setInput(emp)
-  }
+  const editData = (emp) => {
+    setIsEditing(emp.id);
+    setInput(emp);
+  };
   return (
     <>
-      <AddDataToApi fetchData={fetchData} formfield={[input,setInput]} filedEditing={[isEditing, setIsEditing]} />
+      <AddDataToApi
+        fetchData={fetchData}
+        formfield={[input, setInput]}
+        filedEditing={[isEditing, setIsEditing]}
+      />
       <table border={1} className="table p-5 shadow w-75 mx-auto mt-5">
         <thead>
           <tr>
@@ -63,12 +69,18 @@ function App() {
               <td>{emp.city}</td>
               <td>{emp.profile && <img src={emp.profile} alt="" />}</td>
               <td>
-                <button className="btn btn-success" onClick={()=>editData(emp)}>
+                <button
+                  className="btn btn-success"
+                  onClick={() => editData(emp)}
+                >
                   <FontAwesomeIcon icon={faEdit} />
                 </button>
               </td>
               <td>
-                <button className="btn btn-danger" onClick={()=> deleteData(emp.id)}>
+                <button
+                  className="btn btn-danger"
+                  onClick={() => deleteData(emp.id)}
+                >
                   <FontAwesomeIcon icon={faTrash} />
                 </button>
               </td>
