dc43e947d876ad1f8064287f59b9f861181063e1
rahul <nizarerahul7@gmail.com>
2025-03-11 22:09:43 +0530

    firebase onAuthStateChange() for realtime data fetch

--- React/firebase-auth/src/components/Home.jsx
+++ React/firebase-auth/src/components/Home.jsx
@@ -1,24 +1,25 @@
 import React, { useEffect, useState } from "react";
 import { auth, db } from "./firebase/firebase";
 import { doc, getDoc } from "firebase/firestore";
+import { onAuthStateChanged } from "firebase/auth";
 
 const Home = () => {
   const [getUser, setGetUser] = useState(null);
   useEffect(() => {
-    const fetchData = async () => {
-      const user = auth.currentUser;
-      console.log(user?.uid);
-
-      if (user) {
-        const userRef = await getDoc(doc(db, "registeredUser",user.uid ));
-        if(userRef.exists()){
-            setGetUser(userRef.data())
-            console.log(userRef.data())
+    const fetchData = onAuthStateChanged(auth, async(user)=>{
+      if(user){
+        try {
+          const userRef = await getDoc(doc(db,'registeredUser',user.uid))
+          setGetUser(userRef.data())
+        } catch (error) {
+          console.log('error fetching data',error)
         }
+      }else{
+        setGetUser(null)
       }
-    };
-    fetchData();
-  },[]);
+    })
+    return fetchData
+  }, []);
   return (
     <div className="container shadow mt-5 p-5 text-center ">
       <h2 className="mt  fs-1">Home {getUser?.name}</h2>
